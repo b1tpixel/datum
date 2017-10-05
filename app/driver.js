8cc9b4fd189f13961f5abb4e90236655f0877bc5
b1tpixel <gintokius@gmail.com>
2017-10-05 23:16:27 +0300

    v 1.4.
    Исправлен BB

--- app/driver.js
+++ app/driver.js
@@ -3,20 +3,43 @@ var Backbone = require('backbone');
 var Marionette = require('backbone.marionette');
 
 
-var ToDoList = Marionette.LayoutView.extend({
+var mainView = Marionette.LayoutView.extend({
 	el: '#app-hook',
-	template: require('./templates/layout.html')
+	template: require('./templates/layout.html'),
+
+	events: {
+		'click .center': 'clickedOnCenter',
+		'click .arena': 'clickedOnArena',
+		'click .chapel': 'clickedOnChapel',
+		'click .stadium': 'clickedOnStadium'
+	},
+
+	clickedOnCenter:function(){
+		$(".description").hide();
+		$(".center .description").toggle();
+		mymap.panTo(new L.LatLng(47.24187, 39.71069));
+	},
+
+	clickedOnArena:function(){
+		$(".description").hide();
+		$(".arena .description").toggle();
+		mymap.panTo(new L.LatLng(47.24097, 39.70927));
+	},
+
+	clickedOnChapel:function(){
+		$(".description").hide();
+		$(".chapel .description").toggle();
+		mymap.panTo(new L.LatLng(47.23928, 39.71106));
+	},
+
+	clickedOnStadium:function(){
+		$(".description").hide();
+		$(".stadium .description").toggle();
+		mymap.panTo(new L.LatLng(47.23945, 39.70915));
+	}
 });
 
-var todo = new ToDoList({
-  model: new Backbone.Model({
-  	items: [
-    	{item: "Центр парка", desc: "Центральная точка парка ДГТУ", latitude: 47.24187, longitude: 39.71069},
-		{item: "Атлетический манеж ДГТУ", desc: "Манеж в северозападной части парка ДГТУ", latitude: 47.24097, longitude:39.70927},
-		{item: "Церковь", desc: "Храм святой великомученницы Татианы", latitude: 47.23928, longitude: 39.71106},
-		{item: "Футбольный стадион", desc: "Минифутбольный стадион, ограждённый забором", latitude: 47.23945,longitude: 39.70915}
-		]
-	})
-});
+mV = new mainView();
+mV.render();
 
-todo.render();
+
